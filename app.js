0989ac70ad2320aef16b0b60a694957fd7aac4a8
yehoudaamar <242666amar@gmail.com>
2023-02-08 17:04:41 +0200

    variable port

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.get('/', (req, res) => {
 })
 
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running on port 3000 yehouda');
 }
 )
